f03a0ebc64fbb43a241f19290e01966797314958
Flávio Micheletti <flavio.micheletti@midway.com.br>
2020-11-12 22:27:25 -0300

    sugestões

---
+++ tabuadas/crescente.js
@@ -0,0 +1,4 @@
+for (var i = 1; i < 11; i++) {
+    n = 5 * i;
+    console.log("5 x " + i, "=", +n);
+}
---
+++ tabuadas/decrescente.js
@@ -0,0 +1,5 @@
+
+for (var i = 11; i > 0; i--) {
+    n = 5 - i;
+    console.log("5 - " + i, "=", +n);
+}
