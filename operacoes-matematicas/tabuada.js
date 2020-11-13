d65bfd5194a2ac459aa682140a3e7f556bfd1154
Jessika-Martins <jessikam1991@gmail.com>
2020-11-12 22:14:17 -0300

    Arquivo Javascript de Tabuada

---
+++ operacoes-matematicas/tabuada.js
@@ -0,0 +1,11 @@
+
+ 
+    for (var i=1; i<11; i++){
+        n = 5 * i
+        console.log("5 x "+ i, "=", +n) 
+    }
+
+    for (var i=11; i <0; i--){
+        n = 5 - i
+        console.log("5 - "+ i, "=", +n)
+    }
