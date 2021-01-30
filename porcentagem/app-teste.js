10291419ef209f2721b65ab1627262c6655acc99
Flávio Micheletti <flavio.micheletti@midway.com.br>
2021-01-29 21:24:18 -0300

    porcentagem/

--- porcentagem/app-teste.js
+++ porcentagem/app-teste.js
@@ -1,7 +1,8 @@
-const app = require("./app")
+const app = require("./porcentagem")
 
 // Montante
 console.log(app.montante(2, 1200) == 1224)
 
 // Juros
+console.log(app.juros(2, 1200) == 24)
 console.log(app.juros(2, 1200) == 24)
